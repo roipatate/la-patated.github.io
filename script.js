251310247dfc7ebfed742b3fcf10aad2bb9e10a7
ROIpatate <119620955+roipatate@users.noreply.github.com>
2024-12-25 23:46:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -25,17 +25,9 @@ document.addEventListener('DOMContentLoaded', function() {
                 entry.target.classList.remove('visible');
             }
         });
-        function ouvrirLightbox(image) {
-    const lightbox = document.getElementById("lightbox");
-    const lightboxImage = lightbox.querySelector("img");
-
-    lightboxImage.src = image.src; // Utilise la même source que l'image cliquée
-    lightbox.style.display = "flex"; // Affiche la lightbox
-}
-
-function fermerLightbox() {
-    const lightbox = document.getElementById("lightbox");
-    lightbox.style.display = "none"; // Masque la lightbox
-}
-
     }, observerOptions);
+
+    document.querySelectorAll('.encadre').forEach(encadre => {
+        observer.observe(encadre);
+    });
+});
